298ed5bbaba4d11a99ef587306f25937c9896784
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-09-08 19:03:38 +0200

    fix bug in component logic

--- src/components/ConditionalRendering.jsx
+++ src/components/ConditionalRendering.jsx
@@ -2,9 +2,7 @@ function ConditionalRendering(props) {
     const number = props.number;
     return <>
     
-    {(isNaN(number) && <p>Poslani broj nije broj!</p>) || <p>Broj je: {number}</p> }
-
-    {number>5 ? <p>Broj je veći od 5!</p> : <p>Poslani broj je manji od 5!</p>}
+  {!isNaN(number) && (number>5 ? <p>Broj je veći od 5!</p> : <p>Poslani broj je manji od 5!</p>)}
     
     </>;
 }
